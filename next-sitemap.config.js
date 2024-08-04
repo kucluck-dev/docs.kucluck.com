b0e74a579d8b44697168ffe2250998b160ed013a
lukman-nov <novawiyadi@gmail.com>
2024-08-04 15:23:35 +0700

    configuration

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.kucluck.com/',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: './out',
+};
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,40 @@
 import React from "react";
-import {DocsThemeConfig} from "nextra-theme-docs";
+import {DocsThemeConfig, useConfig} from "nextra-theme-docs";
+import {useRouter} from "next/router";
+
+function useHead() {
+  const {asPath} = useRouter();
+  const {frontMatter, title} = useConfig();
+  const url = `https://docs.kucluck.com${asPath}`;
+  const description = frontMatter.description || "Documentation for Kucluck resources for FiveM server";
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link rel="icon" type="image/x-icon" href="/static/logo.ico" />
+      <meta httpEquiv="Content-Language" content="en" />
+      <meta name="description" content={description} />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={description} />
+      <meta name="og:url" content={url} />
+    </>
+  );
+}
+
+function useNextSeoProps() {
+  const {asPath} = useRouter();
+  const arr = asPath.replace(/[-_]/g, " ").split("/");
+  const category = (arr[1][0] !== "#" && arr[1]) || "Kucluck";
+  const rawTitle = arr[arr.length - 1];
+  const title = /[a-z]/.test(rawTitle) && /[A-Z]/.test(rawTitle) ? rawTitle : "%s";
+
+  return {
+    titleTemplate: `${title} - ${rawTitle === category ? "Documentation" : category.replace(/(^\w|\s\w)/g, m => m.toUpperCase())}`,
+  };
+}
 
 const config: DocsThemeConfig = {
+  navigation: true,
   logo: (
     <div
       style={{
@@ -12,7 +45,7 @@ const config: DocsThemeConfig = {
         fontWeight: 550,
       }}
     >
-      Kucluck Documentation
+      Kucluck Docs
     </div>
   ),
   project: {
@@ -25,6 +58,16 @@ const config: DocsThemeConfig = {
   footer: {
     text: "Kucluck",
   },
+  head: useHead,
+  primaryHue: {dark: 200, light: 200},
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  toc: {
+    backToTop: true,
+  },
+
+  useNextSeoProps: useNextSeoProps,
 };
 
 export default config;
